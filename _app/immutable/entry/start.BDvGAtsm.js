8a14ef691871306f145e9674af3da29cc474b1a4
Giuco <danicodes45@gmail.com>
2024-02-19 01:19:51 +0200

    Updates

---
+++ _app/immutable/entry/start.BDvGAtsm.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CqzbKj75.js";export{t as start};
--- src/lib/constants.js
+++
@@ -1,8 +0,0 @@
-const WORK_MAIL="danicodes45@gmail.com";
-export const UBB_BACHELORS="https://www.cs.ubbcluj.ro/education/academic-programmes/undergraduate-programmes/computer-science-programme-profile/"
-export const UBB_MASTERS="https://www.cs.ubbcluj.ro/education/academic-programmes/masters-programmes/distributed-systems-in-internet-programme-profile/"
-export const HERMES="https://societateahermes.ro"
-export const LINKEDIN_URL="https://www.linkedin.com/in/ardelean-george/"
-export const GITHUB_URL="https://github.com/Giuco-senpai45"
-
-export default WORK_MAIL;
